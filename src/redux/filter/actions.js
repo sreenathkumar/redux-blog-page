7717f3151b41b6e5dd8a03c1c616bd830f8ded13
Sreenath <sreenathkumar66@gmail.com>
2022-08-22 19:24:27 +0600

    Add some comments in action creator

--- src/redux/filter/actions.js
+++ src/redux/filter/actions.js
@@ -4,18 +4,23 @@ import {
   FILTER_BY_SEARCH,
 } from "./actionTypes";
 
+//Action for Category filter
 export const filterByCategory = (category) => {
   return {
     type: FILTER_BY_CATEGORY,
     payload: category,
   };
 };
+
+//Action for Author filter
 export const filterByAuthor = (author) => {
   return {
     type: FILTER_BY_AUTHOR,
     payload: author,
   };
 };
+
+//Action for Search filter
 export const filterBySearch = (query) => {
   return {
     type: FILTER_BY_SEARCH,
